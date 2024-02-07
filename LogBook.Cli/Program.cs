64ede540bf9e9c1b6b4c1342d406205d9d76d5d0
Thomas Stöckl <thomas.stoeckl98@gmail.com>
2024-02-07 12:44:15 +0100

    fehlende stunde nachgeholt

--- LogBook.Cli/Program.cs
+++ LogBook.Cli/Program.cs
@@ -3,7 +3,10 @@
 
 Console.WriteLine("Willkomen beim Fahrtenbuch");
 
-IRepository repository = new MemoryRepository();
+string path = "logbook.xml";
+IRepository repository = new XML_Repository(path);
+
+
 
 
 repository.Add(new Entry(DateTime.Now, DateTime.Now.AddHours(2).AddMinutes(22), 25000, 25180, "ZE-XY123", "Zell am See", "München"));
--- LogBook.Lib/XML-Repository.cs
+++ LogBook.Lib/XML-Repository.cs
@@ -9,48 +9,86 @@ namespace LogBook.Lib;
 
 public class XML_Repository : IRepository
 {
-    XElement _rootElement;
+    XElement _rootelement;
+    private string _path;
 
     public XML_Repository(string path)
     {
+        _path = path;
         if (File.Exists(path))
         {
-            _rootElement = XElement.Load(path);
+            _rootelement = XElement.Load(path);
         }
         else
         {
-            _rootElement = new XElement("entries");
+            _rootelement = new XElement("entries");
         }
     }
-
     public bool Add(Entry entry)
     {
-        throw new NotImplementedException();
+        XElement node = new XElement("entry");
+        var idAttrib = new XAttribute("id", entry.Id.ToString());
+        node.Add(idAttrib);
+
+        var startAttrib = new XAttribute("start", entry.Start.ToString());
+        node.Add(startAttrib);
+
+        var endAttrib = new XAttribute("end", entry.End.ToString());
+        node.Add(endAttrib);
+
+        var startkmAttrib = new XAttribute("startkm", entry.StartKM.ToString());
+        node.Add(startkmAttrib);
+
+        var endkmAttrib = new XAttribute("endkm", entry.EndKM.ToString());
+        node.Add(endkmAttrib);
+
+        var nplateAttrib = new XAttribute("numberplate", entry.NumberPlate.ToString());
+        node.Add(nplateAttrib);
+
+        var fromAttrib = new XAttribute("from", entry.From.ToString());
+        node.Add(fromAttrib);
+
+        var toAttrib = new XAttribute("to", entry.To.ToString());
+        node.Add(toAttrib);
+
+        node.Add(entry.Description.ToString());
+        _rootelement.Add(node);
+
+        return this.Save();
     }
 
     public bool Delete(Entry entry)
     {
-        throw new NotImplementedException();
+        var itemsDel = from e in _rootelement.Descendants("entry")
+                       where ((string)e.Attribute("id") ?? "") == entry.Id
+                       select e;
+
+        itemsDel.Remove();
+        return this.Save();
     }
 
     public List<Entry> GetAll()
     {
-        var entries = from entry in this._rootElement.Descendants("entry")
+        var entries = from entry in this._rootelement.Descendants("entry")
                       select entry;
-
-        //TODO:
-        // - Objekt Entry erstellen
-        // - Liste zurückgeben
-
         throw new NotImplementedException();
 
-        //return entries
-
+        // Objekt erstellen
+        // Liste zurückgeben
     }
 
     public bool Save()
     {
-        throw new NotImplementedException();
+        try
+        {
+            _rootelement.Save(_path);
+            return true;
+        }
+        catch (Exception ex)
+        {
+            System.Diagnostics.Debug.WriteLine(ex.Message);
+            return false;
+        }
     }
 
     public bool Update(Entry entry)
@@ -58,5 +96,4 @@ public class XML_Repository : IRepository
         throw new NotImplementedException();
     }
 
-    
 }
