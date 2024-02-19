ee1c7a1e7cd5d5824b8d225183ee93b7013c5cca
Thomas Stöckl <thomas.stoeckl98@gmail.com>
2024-02-19 12:16:47 +0100

    add nuget

--- LogBook.LogBookApp/ViewModels/MainViewModel.cs
+++ LogBook.LogBookApp/ViewModels/MainViewModel.cs
@@ -1,16 +1,29 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using LogBook.Lib;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace LogBook.LogBookApp.ViewModels
 {
-    public partial class MainViewModel : ObservableObject
+    public partial class MainViewModel(IRepository repository) : ObservableObject
     {
 
         public string Header => "Fahrtenbuch";
+        IRepository _repository = repository;
 
+        ObservableCollection<Lib.Entry> _entries = [];
+
+
+
+        [RelayCommand]
+        void LoadData()
+        {
+
+        }
     }
 }
