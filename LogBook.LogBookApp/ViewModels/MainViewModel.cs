9bf719f1b22a162c2493e6adda5a116b6c8470fa
Thomas Stöckl <thomas.stoeckl98@gmail.com>
2024-02-19 12:22:51 +0100

    add behavior

--- LogBook.LogBookApp/ViewModels/MainViewModel.cs
+++ LogBook.LogBookApp/ViewModels/MainViewModel.cs
@@ -8,6 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace LogBook.LogBookApp.ViewModels
 {
     public partial class MainViewModel(IRepository repository) : ObservableObject
